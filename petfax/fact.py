d280e5b48483fc0ba02a2211a39ffc1294f5b515
Rick Dawson <dawsora@gmail.com>
2023-10-30 14:49:49 -0400

    updates to petfax

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,6 +1,14 @@
-from flask import ( Blueprint, render_template ) 
+from flask import ( Blueprint, render_template, request ) 
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
+
+@bp.route('/')
+def index(): 
+    if request.method == "POST":
+        print(request.form)
+        return redirect("/facts")
+    else:
+        return "This is the facts index"
 
 @bp.route('/new')
 def new(): 
